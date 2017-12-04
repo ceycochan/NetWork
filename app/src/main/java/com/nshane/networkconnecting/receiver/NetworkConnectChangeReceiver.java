86fa39010124593ab0a112a56c3bf61f4bcd14a3
ceyco <gumptionchan@163.com>
2017-12-04 11:12:53 +0800

    initial fix

---
+++ app/src/main/java/com/nshane/networkconnecting/receiver/NetworkConnectChangeReceiver.java
@@ -0,0 +1,17 @@
+package com.nshane.networkconnecting.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Administrator on 2017/11/18 0018.
+ */
+
+public class NetworkConnectChangeReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //
+    }
+}
